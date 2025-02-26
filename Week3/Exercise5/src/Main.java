4e2f1beaf0618da6d0eaf7ee6e1444c04841d5ab
thristovaa <raya.thristova@gmail.com>
2025-02-26 16:53:44 +0100

    [Week 3] Exercise 5 - birthday countdown

--- Week3/Exercise5/src/Main.java
+++ Week3/Exercise5/src/Main.java
@@ -1,5 +1,7 @@
+import java.time.Duration;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
+import java.time.LocalTime;
 import java.time.format.DateTimeFormatter;
 
 public class Main {
@@ -7,7 +9,7 @@ public class Main {
         //now
         LocalDateTime now = LocalDateTime.now();
 
-        LocalDate birthday = LocalDate.of(2005, 12, 22);
+        LocalDate birthday = LocalDate.of(2025, 12, 22);
         LocalDate today = LocalDate.now();
 
         //birthday
@@ -17,9 +19,21 @@ public class Main {
             nextBirthday = nextBirthday.plusYears(1);
         }
 
+        //waiting time
+        LocalDateTime momentOfMyBirthday = LocalDateTime.of(birthday, LocalTime.of(0,0));
+
+        Duration waitingTime = Duration.between(now, momentOfMyBirthday);
+
+        //output
         System.out.println(now);
         System.out.println(getProperDate(now));
         System.out.println("Your next birthday will be at " + nextBirthday);
+        System.out.printf("Your next birthday is in: %d days, %d hours, %d minutes and %d seconds%n",
+                waitingTime.toDays(),
+                waitingTime.toHoursPart(),
+                waitingTime.toMinutesPart(),
+                waitingTime.toSecondsPart()
+        );
     }
 
     public static String getProperDate(LocalDateTime now){
