097f05fb46b6fe5d7757e5d93b8d1511cbcc3150
thristovaa <raya.thristova@gmail.com>
2025-03-03 16:58:32 +0100

    [Week 4] Exercise 1

---
+++ Week4/inClass/src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        Person p = new Person("Raya", "Hristova");
+
+        Student s = new Student("Random", "Name", 1111);
+
+        Person tristan = new Teacher("Tristan", "Pothoven");
+    }
+}
